e193fe5d927fadbe688a099e7070b08c2dc70c60
ravitheja <ravi.edimudi@gmail.com>
2018-07-25 14:56:57 +0530

    new project

---
+++ Amazon_Project/automation/scripts/test_case_01.py
@@ -0,0 +1,30 @@
+'''
+Created on Jul 16, 2018
+
+@author: Raghu
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    
+    
+    def test_case_01(self):
+        print("My First Test Case Done")
+        
+        
+        
+
+
+
+obj1 = MyClass()
+obj1.test_case_01()
+
---
+++ Amazon_Project/selenium/common/lib/config.py
@@ -0,0 +1,5 @@
+'''
+Created on Jul 16, 2018
+
+@author: integra
+'''
